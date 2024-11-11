919f61edbb5c15f772c6bb0ade1d771424c7513a
ohpie69 <bencadayona@gmail.com>
2024-11-11 14:06:30 +0800

    himo tag bag.o

---
+++ src/main/java/com/CSIT321/Hkotisk/Config/SecurityConfiguration.java
@@ -0,0 +1,61 @@
+package com.CSIT321.Hkotisk.Config;
+
+import com.CSIT321.Hkotisk.Filter.JwtFilter;
+import com.CSIT321.Hkotisk.Service.MyUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Autowired
+    private MyUserDetailService userDetailsService;
+
+    @Autowired
+    JwtFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/resources/**", "/static/**", "/public/**", "/auth/**", "/auth/signup", "/auth/signin", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/login").permitAll()
+                        .requestMatchers("/user/**", "/auth/signout", "/user/products", "/user/addToCart", "/user/cart", "/user/order").hasAnyAuthority("ROLE_STUDENT", "ROLE_STAFF")
+                        .requestMatchers("/staff/**", "/auth/signout", "/staff/product", "/staff/orders", "/staff/order").hasAuthority("ROLE_STAFF")
+                        .anyRequest().authenticated())
+                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+
+    }
+}
---
+++ src/main/java/com/CSIT321/Hkotisk/Response/ServerResponse.java
@@ -0,0 +1,12 @@
+package com.CSIT321.Hkotisk.Response;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ServerResponse {
+    private String status;
+    private String message;
+    private String authToken;
+}
